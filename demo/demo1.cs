d546b69386320325ca241b573310b798f25863f6
Prem119L <premsagarprathipati@gmail.com>
2022-12-13 11:26:23 +0530

    prem sagar demo project

---
+++ demo/demo1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    internal class demo1
+    {
+        static void test()
+        {
+            int num1, num2, sum;
+            Console.WriteLine("calculating the sum of the numbers:");
+            Console.WriteLine("enter the num1 value:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the num2 value:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            sum = num1 + num2;
+            Console.WriteLine("sum of the two numbers is:"+sum);
+
+        }
+    }
+}
